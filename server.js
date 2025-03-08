5ce9d037fa3f08e164c7540a8ed0be9ed122e0cb
Ravirkt <r_tjikhoeri@live.com>
2025-03-08 19:54:13 +0100

    added filter for location elmelo #3 #2

--- server.js
+++ server.js
@@ -66,6 +66,14 @@ app.get('/Eindhoven', async function (request, response) {
   response.render('index.liquid', { events: apiResponseJSON.data })
 })
 
+app.get('/Ermelo', async function (request, response) {
+  const apiResponse = await fetch('https://fdnd-agency.directus.app/items/dda_events?filter={"photo":{"_neq":null},"location":{"_eq":"Ermelo"}}')
+  const apiResponseJSON = await apiResponse.json()
+  console.log(apiResponseJSON.data)
+
+  response.render('index.liquid', { events: apiResponseJSON.data })
+})
+
 
 // Maak een POST route voor de index; hiermee kun je bijvoorbeeld formulieren afvangen
 // Hier doen we nu nog niets mee, maar je kunt er mee spelen als je wilt
